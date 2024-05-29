6012667bd41252da5bb0f7b033c98a71046c5b89
Mikhail Bakulin <mibak@yandex.ru>
2024-05-29 16:36:44 +0400

    Realized save method in SessionsDao layer.

--- src/main/java/dao/SessionsDao.java
+++ src/main/java/dao/SessionsDao.java
@@ -1,4 +1,23 @@
 package dao;
 
+import model.UserSession;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import utils.HibernateUtil;
+
 public class SessionsDao {
+
+    public void save(UserSession userSession) throws HibernateException {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(userSession);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
 }
