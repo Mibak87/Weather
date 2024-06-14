82b89dd35573d7ea7f09f31f06059891140217fa
Mikhail Bakulin <mibak@yandex.ru>
2024-06-14 12:56:35 +0400

    Add the ThymeleafUtil configure class.

--- src/main/java/utils/ThymeleafUtil.java
+++ src/main/java/utils/ThymeleafUtil.java
@@ -1,8 +1,50 @@
 package utils;
 
 
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
+import org.thymeleaf.web.IWebApplication;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
+@WebListener
+public class ThymeleafUtil implements ServletContextListener {
 
-public class ThymeleafUtil {
+    public static final String TEMPLATE_ENGINE_ATTR = "TemplateEngineInstance";
 
+    private ITemplateEngine templateEngine;
+
+    private JakartaServletWebApplication application;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        this.application = JakartaServletWebApplication.buildApplication(sce.getServletContext());
+        this.templateEngine = templateEngine(this.application);
+
+        sce.getServletContext().setAttribute(TEMPLATE_ENGINE_ATTR, templateEngine);
+    }
+
+    private ITemplateEngine templateEngine(IWebApplication application) {
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        WebApplicationTemplateResolver templateResolver = templateResolver(application);
+        templateEngine.setTemplateResolver(templateResolver);
+
+        return templateEngine;
+    }
+
+    private WebApplicationTemplateResolver templateResolver(IWebApplication application) {
+        WebApplicationTemplateResolver templateResolver = new WebApplicationTemplateResolver(application);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCacheTTLMs(3600000L);
+        templateResolver.setCacheable(false);
+
+        return templateResolver;
+    }
 }
