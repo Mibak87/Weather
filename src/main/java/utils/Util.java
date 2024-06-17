7e0c521a91519a3ee8310fd43411b172bf491cea
Mikhail Bakulin <mibak@yandex.ru>
2024-06-17 10:48:43 +0400

    Add getApi Util methode for getting API url.

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -9,4 +9,12 @@ public class Util {
         long updatedTimeInMillis = currentDate.getTime() + (HOURS_BEFORE_SESSION_TIME_EXPIRES * 60 * 60 * 1000);
         return new Date(updatedTimeInMillis);
     }
+
+    public static String getApiUrl(String location) {
+        String apiKey = "dc6eaa0000964813c5c502600f228fa2";
+        String url = "https://api.openweathermap.org/data/2.5/weather?";
+        String units = "&units=metric";
+        String apiUrl = url + "q=" + location + "&appid=" + apiKey + units;
+        return apiUrl;
+    }
 }
