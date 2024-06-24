44d2ba15ea6c842a8c2a6bc5de817e48b715ae22
Mikhail Bakulin <mibak@yandex.ru>
2024-06-24 16:32:00 +0400

    Add: DeleteLocationController for delete the Location of User from BD.

---
+++ src/main/java/controller/DeleteLocationController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import service.LocationService;
+
+import java.io.IOException;
+
+@WebServlet(name = "DeleteLocationController", value = "/deletelocation")
+public class DeleteLocationController extends HttpServlet {
+    private static final Logger logger = LogManager.getLogger(DeleteLocationController.class);
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        long locationId = Long.parseLong(request.getParameter("locationId"));
+        String userLogin = request.getParameter("userLogin");
+        new LocationService().deleteLocation(locationId,userLogin);
+        response.sendRedirect("weather");
+    }
+}
