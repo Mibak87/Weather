b685fb7e166981b3d34d377688d24df00e481eb7
Mikhail Bakulin <mibak@yandex.ru>
2024-07-01 11:29:28 +0400

    Add: Add H2 Data Base for integration testing and dependencies for H2 in pom.xml. Add hibernate configure file for H2. Add HibernateH2Util class for testing. Rename hibernatePostgres.cfg.xml for main application.

--- src/test/java/utils/HibernateH2Util.java
+++ src/test/java/utils/HibernateH2Util.java
@@ -3,12 +3,12 @@ package utils;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class HibernateUtil {
+public class HibernateH2Util {
     private static SessionFactory sessionFactory;
 
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
-            sessionFactory = new Configuration().configure("hibernatePostgres.cfg.xml").buildSessionFactory();
+            sessionFactory = new Configuration().configure("hibernateH2.cfg.xml").buildSessionFactory();
         }
         return sessionFactory;
     }
