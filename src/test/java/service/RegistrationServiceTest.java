d11d55a9e7d7682649546abf9cebbc7cfb723f63
Mikhail Bakulin <mibak@yandex.ru>
2024-07-05 10:42:59 +0400

    Add: Add integration test of the throw UserAlreadyExistsException when registering a non-unique user.

--- src/test/java/service/RegistrationServiceTest.java
+++ src/test/java/service/RegistrationServiceTest.java
@@ -1,9 +1,10 @@
 package service;
 
+import exceptions.UserAlreadyExistsException;
 import model.User;
 import model.UserSession;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import utils.HibernateUtil;
@@ -17,34 +18,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class RegistrationServiceTest {
 
     private UserSession userSession;
+    private RegistrationService registrationService;
     @BeforeEach
     void init() {
         User user = new User("Vasya","123");
         Date date = new Date(new Date().getTime()-5000);
         userSession = new UserSession("1",user, date);
+        registrationService = new RegistrationService();
     }
 
     @Test
     void registration_saveUserSession_saveUser() {
-        new RegistrationService().registration(userSession);
+        registrationService.registration(userSession);
         Session session = HibernateUtil.getSessionFactory().openSession();
         List<User> userList = session.createQuery("FROM User", User.class).getResultList();
         session.close();
         assertEquals(1, userList.size());
     }
 
-    /*@Test
+    @Test
     void registration_saveUserSessionWithNotUniqueUser_UserAlreadyExistsException() {
-        Session session = HibernateH2Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.persist(userSession);
-        transaction.commit();
+        registrationService.registration(userSession);
         User notUniqueUser = new User("Vasya","456");
         Date date = new Date(new Date().getTime());
         UserSession newUserSession = new UserSession("1",notUniqueUser, date);
-        transaction = session.beginTransaction();
-        session.persist(newUserSession);
-        transaction.commit();
-        session.close();
-    }*/
+        Assertions.assertThrows(UserAlreadyExistsException.class, () -> {
+            registrationService.registration(newUserSession);
+        });
+    }
 }
