4924598c8ecfecee5a60664024be48ad0e6554f8
Mikhail Bakulin <mibak@yandex.ru>
2024-07-29 13:48:14 +0400

    Refactored: Delete session authentication logic from the FindLocationController.

--- src/main/java/controller/FindLocationController.java
+++ src/main/java/controller/FindLocationController.java
@@ -1,20 +1,14 @@
 package controller;
 
-import dao.SessionsDao;
 import dto.CitiesResponseDto;
 import exceptions.ErrorApiConnectionException;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-import model.UserSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import service.OpenWeatherApiService;
 
 import java.io.IOException;
-import java.net.http.HttpClient;
-import java.util.Date;
-import java.util.Optional;
 
 @WebServlet(name = "FindLocationController", value = "/location")
 public class FindLocationController extends BaseController {
@@ -26,32 +20,23 @@ public class FindLocationController extends BaseController {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String sessionId = request.getSession().getId();
-        Optional<UserSession> optionalUserSession = new SessionsDao().findById(sessionId);
-        if (optionalUserSession.isEmpty()) {
-            response.sendRedirect("authorization");
+        String location = request.getParameter("location");
+        context.setVariable("userName", request.getAttribute("login"));
+        if (location.isEmpty()) {
+            context.setVariable("error", "Поле ввода пустое. Введите название города.");
+            templateEngine.process("location", context, response.getWriter());
         } else {
-            UserSession userSession = optionalUserSession.get();
-            if (new Date().after(userSession.getExpiresAt())) {
-                response.sendRedirect("authorization");
-            } else {
-                String location = request.getParameter("location");
-                if (location.isEmpty()) {
-                    context.setVariable("error", "Поле ввода пустое. Введите название города.");
-                    templateEngine.process("location", context, response.getWriter());
-                } else {
-                    try {
-                        CitiesResponseDto[] citiesResponseDto = openWeatherApiService.getCities(location);
-                        logger.info(citiesResponseDto.toString());
-                        context.setVariable("userName", userSession.getUser().getLogin());
-                        context.setVariable("citiesResponse", citiesResponseDto);
-                        templateEngine.process("location", context, response.getWriter());
-                    } catch (ErrorApiConnectionException e) {
-                        context.setVariable("error", "Не удается подключиться к серверу с погодой.");
-                        templateEngine.process("location", context, response.getWriter());
-                    }
-                }
+            try {
+                CitiesResponseDto[] citiesResponseDto = openWeatherApiService.getCities(location);
+                logger.info(citiesResponseDto.toString());
+                context.setVariable("citiesResponse", citiesResponseDto);
+                templateEngine.process("location", context, response.getWriter());
+            } catch (ErrorApiConnectionException e) {
+                context.setVariable("error", "Не удается подключиться к серверу с погодой.");
+                templateEngine.process("location", context, response.getWriter());
             }
         }
+
+
     }
 }
